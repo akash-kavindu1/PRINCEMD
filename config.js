5c5063e4c08d8366a0dcd3142b03ba28782a770e
akash-kavindu1 <akashnimsara444@gmail.com>
2024-11-27 23:29:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 //BETA: If you want to avoid typing the number that will be bot into the console, I added from here then:
 //Only applies to option 2 (be a bot with an 8-digit text code)
 
-global.botNumberCode = "" //Example: +923092668108
-global.confirmCode = "" 
+global.botNumberCode = "+94771810698" //Example: +923092668108
+global.confirmCode = "akashabc" 
 
 
 //💌------------------------------------------💌
